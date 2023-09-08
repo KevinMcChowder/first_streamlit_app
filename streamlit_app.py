a5d3740696ad45abd81b24115ef84f5fbaa14992
KevinMcChowder <144151910+KevinMcChowder@users.noreply.github.com>
2023-09-08 12:49:13 -0500

    Update streamlit_app.py
    remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
 #Add fruit to list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add Fruit to the List'):
